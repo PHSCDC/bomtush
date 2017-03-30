a0e44e30bb8eed35de7e15285b12eee606a17839
liquidsmoke774 <tjohnbush@gmail.com>
2017-03-30 16:24:31 -0500

    Update getposts.php

--- getposts.php
+++ getposts.php
@@ -39,4 +39,31 @@ function getNewestPosts($offset)
 		echo 'Connection failed: ' . $e->getMessage();
 	}
 }
+
+function getUserPosts($user, $offset)
+{
+	include "config.php";
+	$offset=($offset-1)*25;
+	try {
+		$conn = new PDO($db, $user, $pass);
+			
+		
+				
+				$input = $conn->prepare("SELECT *, DATEDIFF(CURTIME(), date) AS daysAgo, EXTRACT(HOUR FROM CURTIME()) - EXTRACT(HOUR FROM date) AS hoursAgo, EXTRACT(MINUTE FROM CURTIME()) - EXTRACT(MINUTE FROM date) AS minutesAgo FROM post WHERE author='$user' ORDER BY date DESC LIMIT 25 OFFSET $offset;");
+			
+			
+
+		if(!$input->execute())
+		{
+			echo "Failed to read posts: " . $input->errorInfo()[0] . " " . $input->errorInfo()[2] . "<br>";
+		}
+
+		$conn = null;
+
+		return $input->fetchAll();
+	}
+	catch (PDOException $e) {
+		echo 'Connection failed: ' . $e->getMessage();
+	}
+}
 ?>
