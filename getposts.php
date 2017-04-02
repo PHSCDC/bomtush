815365ba9ec142a6f7737de1e5b2bda6ec0319a7
Jimmy <jimmyk3.14159@gmail.com>
2017-04-02 14:45:13 -0500

    Update getposts.php

--- getposts.php
+++ getposts.php
@@ -41,7 +41,7 @@ function getUserPosts($name, $offset) {
 	$offset=($offset-1)*25;
 	try {
 		$conn = new PDO($db, $user, $pass);
-		$input = $conn->prepare("SELECT *, DATEDIFF(CURTIME(), date) AS daysAgo, EXTRACT(HOUR FROM CURTIME()) - EXTRACT(HOUR FROM date) AS hoursAgo, EXTRACT(MINUTE FROM CURTIME()) - EXTRACT(MINUTE FROM date) AS minutesAgo FROM post WHERE author=" . $conn->quote($name) . " ORDER BY date DESC LIMIT 25 OFFSET $offset;");
+		$input = $conn->prepare("SELECT *, DATEDIFF(CURTIME(), date) AS daysAgo, EXTRACT(HOUR FROM CURTIME()) - EXTRACT(HOUR FROM date) AS hoursAgo, EXTRACT(MINUTE FROM CURTIME()) - EXTRACT(MINUTE FROM date) AS minutesAgo FROM post WHERE author=" . $conn->quote($name) . " ORDER BY date DESC LIMIT 25 OFFSET $conn->quote($offset);");
 		if (!$input->execute()) {
 			echo "Failed to read posts: " . $input->errorInfo()[0] . " " . $input->errorInfo()[2] . "<br>";
 		}
