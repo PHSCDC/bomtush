0c9f08ef75ab852170557208ad23b2060f35ed06
Jimmy <jimmyk3.14159@gmail.com>
2017-04-02 14:42:32 -0500

    Update getposts.php

--- getposts.php
+++ getposts.php
@@ -7,20 +7,20 @@ function getNewestPosts($offset) {
 		if (isset($_GET["sort"])) {
 			switch ($_GET["sort"]) {
 				case 1:
-					$input = $conn->prepare("SELECT *, DATEDIFF(CURTIME(), date) AS daysAgo, EXTRACT(HOUR FROM CURTIME()) - EXTRACT(HOUR FROM date) AS hoursAgo, EXTRACT(MINUTE FROM CURTIME()) - EXTRACT(MINUTE FROM date) AS minutesAgo FROM post ORDER BY date DESC LIMIT 25 OFFSET $offset;");
+					$input = $conn->prepare("SELECT *, DATEDIFF(CURTIME(), date) AS daysAgo, EXTRACT(HOUR FROM CURTIME()) - EXTRACT(HOUR FROM date) AS hoursAgo, EXTRACT(MINUTE FROM CURTIME()) - EXTRACT(MINUTE FROM date) AS minutesAgo FROM post ORDER BY date DESC LIMIT 25 OFFSET $conn->quote($offset);");
 					break;
 				case 2:
-					$input = $conn->prepare("SELECT *, DATEDIFF(CURTIME(), date) AS daysAgo, EXTRACT(HOUR FROM CURTIME()) - EXTRACT(HOUR FROM date) AS hoursAgo, EXTRACT(MINUTE FROM CURTIME()) - EXTRACT(MINUTE FROM date) AS minutesAgo FROM post ORDER BY date ASC LIMIT 25 OFFSET $offset;");
+					$input = $conn->prepare("SELECT *, DATEDIFF(CURTIME(), date) AS daysAgo, EXTRACT(HOUR FROM CURTIME()) - EXTRACT(HOUR FROM date) AS hoursAgo, EXTRACT(MINUTE FROM CURTIME()) - EXTRACT(MINUTE FROM date) AS minutesAgo FROM post ORDER BY date ASC LIMIT 25 OFFSET $conn->quote($offset);");
 					break;
 				case 3:
-					$input = $conn->prepare("SELECT *, DATEDIFF(CURTIME(), date) AS daysAgo, EXTRACT(HOUR FROM CURTIME()) - EXTRACT(HOUR FROM date) AS hoursAgo, EXTRACT(MINUTE FROM CURTIME()) - EXTRACT(MINUTE FROM date) AS minutesAgo FROM post ORDER BY rand() DESC LIMIT 25 OFFSET $offset;");
+					$input = $conn->prepare("SELECT *, DATEDIFF(CURTIME(), date) AS daysAgo, EXTRACT(HOUR FROM CURTIME()) - EXTRACT(HOUR FROM date) AS hoursAgo, EXTRACT(MINUTE FROM CURTIME()) - EXTRACT(MINUTE FROM date) AS minutesAgo FROM post ORDER BY rand() DESC LIMIT 25 OFFSET $conn->quote($offset);");
 					break;
 				default:
-					$input = $conn->prepare("SELECT *, DATEDIFF(CURTIME(), date) AS daysAgo, EXTRACT(HOUR FROM CURTIME()) - EXTRACT(HOUR FROM date) AS hoursAgo, EXTRACT(MINUTE FROM CURTIME()) - EXTRACT(MINUTE FROM date) AS minutesAgo FROM post ORDER BY date DESC LIMIT 25 OFFSET $offset;");
+					$input = $conn->prepare("SELECT *, DATEDIFF(CURTIME(), date) AS daysAgo, EXTRACT(HOUR FROM CURTIME()) - EXTRACT(HOUR FROM date) AS hoursAgo, EXTRACT(MINUTE FROM CURTIME()) - EXTRACT(MINUTE FROM date) AS minutesAgo FROM post ORDER BY date DESC LIMIT 25 OFFSET $conn->quote($offset);");
 					break;
 			}
 		} else {
-			$input = $conn->prepare("SELECT *, DATEDIFF(CURTIME(), date) AS daysAgo, EXTRACT(HOUR FROM CURTIME()) - EXTRACT(HOUR FROM date) AS hoursAgo, EXTRACT(MINUTE FROM CURTIME()) - EXTRACT(MINUTE FROM date) AS minutesAgo FROM post ORDER BY date DESC LIMIT 25 OFFSET $offset;");
+			$input = $conn->prepare("SELECT *, DATEDIFF(CURTIME(), date) AS daysAgo, EXTRACT(HOUR FROM CURTIME()) - EXTRACT(HOUR FROM date) AS hoursAgo, EXTRACT(MINUTE FROM CURTIME()) - EXTRACT(MINUTE FROM date) AS minutesAgo FROM post ORDER BY date DESC LIMIT 25 OFFSET $conn->quote($offset);");
 		}
 
 		if(!$input->execute()) {
@@ -36,12 +36,12 @@ function getNewestPosts($offset) {
 	}
 }
 
-function getUserPosts($user, $offset) {
+function getUserPosts($name, $offset) {
 	include "config.php";
 	$offset=($offset-1)*25;
 	try {
 		$conn = new PDO($db, $user, $pass);
-		$input = $conn->prepare("SELECT *, DATEDIFF(CURTIME(), date) AS daysAgo, EXTRACT(HOUR FROM CURTIME()) - EXTRACT(HOUR FROM date) AS hoursAgo, EXTRACT(MINUTE FROM CURTIME()) - EXTRACT(MINUTE FROM date) AS minutesAgo FROM post WHERE author='$user' ORDER BY date DESC LIMIT 25 OFFSET $offset;");
+		$input = $conn->prepare("SELECT *, DATEDIFF(CURTIME(), date) AS daysAgo, EXTRACT(HOUR FROM CURTIME()) - EXTRACT(HOUR FROM date) AS hoursAgo, EXTRACT(MINUTE FROM CURTIME()) - EXTRACT(MINUTE FROM date) AS minutesAgo FROM post WHERE author=" . $conn->quote($name) . " ORDER BY date DESC LIMIT 25 OFFSET $offset;");
 		if (!$input->execute()) {
 			echo "Failed to read posts: " . $input->errorInfo()[0] . " " . $input->errorInfo()[2] . "<br>";
 		}
